12f5d35069276e4b5ddab2d499d04cc56b041759
Yury Barysiuk <yura.barysiuk@gmail.com>
2023-02-24 00:00:43 +0300

    feat: install webpack-dev-server

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+#### 1.0.0 (2023-02-23)
+
+##### New Features
+
+*  webpack install (a660f1d9)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+const path = require('path');
 
+
+const isDev = process.env.NODE_ENV === 'development';
+isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name]${ext}` : `[query][contenthash]${ext}`
+
+console.log('isDev: ', isDev)
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: filename('.js'),
+        path: path.resolve(__dirname, 'dist')
+    }
 }
