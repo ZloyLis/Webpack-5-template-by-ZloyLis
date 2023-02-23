af1f14593a51e0ccbe37eeefe4e2cf87d148247c
Yury Barysiuk <yura.barysiuk@gmail.com>
2023-02-24 00:43:58 +0300

    feat: install html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const isDev = process.env.NODE_ENV === 'development';
 isProd = !isDev;
 
-const filename = (ext) => isDev ? `[name]${ext}` : `[query][contenthash]${ext}`
+const filename = (ext) => isDev ? `[name]${ext}` : `[contenthash]${ext}`
 
 console.log('isDev: ', isDev)
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: './src/index.js',
     output: {
         filename: filename('.js'),
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '.src/index.html'),
+            minify: {
+                collapseWhitespace: isProd,
+            }
+        }),
+    ]
 }
