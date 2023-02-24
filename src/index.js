42acbb0663d0663c7e10f5504bce7bdc99a2592e
Yury Barysiuk <yura.barysiuk@gmail.com>
2023-02-24 04:44:52 +0300

    feat: install loaders and dev-server
    Loaders:
    * xml-loader
    * csv-loader
    * postcss-preset-env
    Plugins:
    * install and setting dev-server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './js/testtest/other'
+import './js/testtest/other.js'
+import DB from './assets/json.json';
+import './styles/styles.scss'
+
+
+const body = document.querySelector('body');
+body.style.backgroundImage = "url('./assets/images/bg.jpg')"
+
+
+console.log(DB.title)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -17,6 +18,7 @@ module.exports = {
     output: {
         filename: `js/` + filename('.js'),
         path: path.resolve(__dirname, 'dist'),
+        /*assetModuleFilename: `./assets/` + filename('[ext]'),*/
         clean: true,
     },
 
@@ -28,8 +30,24 @@ module.exports = {
             minify: {
                 collapseWhitespace: isProd,
             },
-        })
+        }),
+        //new CopyWebpackPlugin(),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        historyApiFallback: true,
+        liveReload: true,
+        compress: true,
+        port: 9000,
+        hot: true
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 512000,
+        maxEntrypointSize: 512000
+    },
 
     module:  {
         rules:[
@@ -37,6 +55,44 @@ module.exports = {
                 test: /\.(sa|sc|c)ss$/i,
                 use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
             },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './assets/images/[name][ext]'
+                }
+            },
+            {
+                test: /\.(ico)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './assets/icons/[name][ext]'
+                }
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './assets/svg/[name][ext]'
+                }
+
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './assets/fonts/[name][ext]'
+                }
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+
         ]
     }
 }
