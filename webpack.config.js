87818697d8ece1fa150ae93cab1622d7ccdcccc8
Yury Barysiuk <yura.barysiuk@gmail.com>
2023-03-02 22:28:52 +0300

    Revert "fix: enable experiments function - `topLevelAwait: true`"
    This reverts commit 4baf4c57d692987943aa7a1e35f8485368bcd656.

--- webpack.config.js
+++ webpack.config.js
@@ -81,9 +81,6 @@ module.exports = {
     filename: `js/${filename('.js')}`,
     path: path.resolve(__dirname, 'dist'),
   },
-  experiments: {
-    topLevelAwait: true
-  },
 
   devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
 
